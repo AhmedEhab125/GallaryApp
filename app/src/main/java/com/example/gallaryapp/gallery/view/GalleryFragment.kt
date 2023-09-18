94c2a89f1f21cdb79f867ed1cf1b0bdbe6922d66
AhmedEhab125 <121432823+AhmedEhab125@users.noreply.github.com>
2023-09-18 14:37:23 +0300

    check for permission

--- app/src/main/java/com/example/gallaryapp/gallery/view/GalleryFragment.kt
+++ app/src/main/java/com/example/gallaryapp/gallery/view/GalleryFragment.kt
@@ -1,11 +1,16 @@
 package com.example.gallaryapp.gallery.view
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.database.Cursor
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -17,12 +22,11 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-
 class GalleryFragment : Fragment() {
-
+    private val STORAGE_PERMISSION_CODE = 1
     private lateinit var binding: FragmentGalleryBinding
     private lateinit var adapter: GalleryAdapter
-    val galleryViewModel: GalleryViewModel by viewModels()
+    private val galleryViewModel: GalleryViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -36,7 +40,18 @@ class GalleryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setUI()
-        getGalleryImgs()
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE)
+            == PackageManager.PERMISSION_GRANTED) {
+            getGalleryImgs()
+        } else {
+            // Permission is not granted
+            // Request the permission
+            requestPermissions(
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                STORAGE_PERMISSION_CODE
+            )
+        }
+
     }
 
     fun setUI() {
@@ -55,6 +70,25 @@ class GalleryFragment : Fragment() {
         }
 
     }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, you can now access external storage
+                getGalleryImgs()
+                println("d5aaaaaaaaaaaaal")
+            } else {
+                // Permission denied, handle it accordingly (e.g., show a message to the user)
+                Toast.makeText(requireContext(),"enable access storage",Toast.LENGTH_LONG).show()
+                println("d5aaaaaaaaaaaaal")
+                println("d5aaaaaaaaaaaaal")
+            }
+        }
+    }
 
 }
